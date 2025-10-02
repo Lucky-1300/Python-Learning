64f7d254f2ab69e2ee87f563470e09e270776240
Lucky-1300 <youremail@example.com>
2025-10-02 18:49:14 +0530

    vhanged

--- condition.py
+++ condition.py
@@ -1,34 +1,67 @@
 # Positive, Negative, or Zero
 # Write a program to check whether a number is positive, negative, or zero.
 
-num = int(input())
-if num > 0:
-    print(num,"is positive number")
-elif num < 0:
-    print(num,"is negative number")
-else:
-    print("zero")
+# num = int(input())
+# if num > 0:
+#     print(num,"is positive number")
+# elif num < 0:
+#     print(num,"is negative number")
+# else:
+#     print("zero")
 
 
 
 # Even or Odd
 # Write a program to check if a number is even or odd.
 
-num = int(input())
-if num % 2 == 0:
-    print("even")
-else:
-    print("odd")
+# num = int(input())
+# if num % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
 
 
 # Maximum of Two Numbers
 # Write a program to find the largest of two numbers.
 
-A = int(input())
-B = int(input())
-if A > B:
-    print(A,"is largest")
-elif A < B:
-    print(B,"is largest")
-else:
-    print("both are equall")
+# A = int(input())
+# B = int(input())
+
+# if A > B:
+#     print(A,"is largest")
+# elif A < B:
+#     print(B,"is largest")
+# else:
+#     print("both are equall")
+
+
+
+# Maximum of Three Numbers
+# Write a program to find the largest of three numbers.
+
+# num1 = int(input())
+# num2 = int(input())
+# num3 = int(input())
+
+# if num1 > num2 and num1 > num3:
+#     print(num1)
+# elif num2 > num1 and num2 > num3:
+#     print(num2)
+# else:
+#     print(num3)
+
+
+
+# Leap Year Check
+# Write a program to check whether a given year is a leap year or not.
+
+
+
+# year = int(input())
+# if (year % 400 == 0 ):
+#     print("leap year")
+# elif year % 4 == 0 and year % 100 != 0:
+#     print("yes it is a leap year")
+# else:
+#     print("its not a leap year")
+
